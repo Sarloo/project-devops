e2f3199646e2b843c31db934c9aede604f488d77
Sarloo <carloscecor05@gmail.com>
2026-03-31 22:10:32 +0000

    feat: validacion de parametgit add ec2/gestionar_ec2.py

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -35,3 +35,34 @@ def terminar_instancia(instance_id):
     ec2 = boto3.client("ec2")
     ec2.terminate_instances(InstanceIds=[instance_id])
     print(f"Instancia {instance_id} terminada correctamente.")
+
+    def main():
+    if len(sys.argv) < 2:
+        mostrar_uso()
+        sys.exit(1)
+
+    accion = sys.argv[1]
+
+    if accion == "listar":
+        listar_instancias()
+    elif accion in ["iniciar", "detener", "terminar"]:
+        if len(sys.argv) < 3:
+            print("Error: falta el instance_id.")
+            mostrar_uso()
+            sys.exit(1)
+
+        instance_id = sys.argv[2]
+
+        if accion == "iniciar":
+            iniciar_instancia(instance_id)
+        elif accion == "detener":
+            detener_instancia(instance_id)
+        elif accion == "terminar":
+            terminar_instancia(instance_id)
+    else:
+        print("Acción no válida.")
+        mostrar_uso()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
